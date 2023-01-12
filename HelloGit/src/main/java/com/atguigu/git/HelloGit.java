73291075bd2ba29838925e5d526dfde1aac5a26d
Mysa <1055052527@qq.com>
2023-01-12 22:47:53 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author FHYstart
+ * @create 2023-01-12-22:39
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
